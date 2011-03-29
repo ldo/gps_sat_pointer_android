2cfbdc287733127940c9b5fd171941b5cad4bdd9
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2011-03-29 03:09:56 +0000

    dump more info

--- src/nz/gen/geek_central/SensorTest/Main.java
+++ src/nz/gen/geek_central/SensorTest/Main.java
@@ -44,7 +44,30 @@ public class Main extends android.app.Activity
                     ThisProvider.supportsBearing(),
                     ThisProvider.supportsSpeed()
                   );
+                final android.location.Location LastKnown =
+                    Locator.getLastKnownLocation(ProviderName);
+                if (LastKnown != null)
+                  {
+                    final StringBuilder Dump = new StringBuilder();
+                    LastKnown.dump(new android.util.StringBuilderPrinter(Dump), "\n  loc:");
+                    System.err.printf
+                      (
+                        " last known location: %s\n",
+                        Dump.toString()
+                      );
+                  }
+                else
+                  {
+                    System.err.println(" no last known location");
+                  } /*if*/
               } /*for*/
+            final android.location.GpsStatus GPS = Locator.getGpsStatus(null);
+            System.err.printf
+              (
+                "GPS status: max sats = %d, time to first fix = %d\n",
+                GPS.getMaxSatellites(),
+                GPS.getTimeToFirstFix()
+              );
           }
         else
           {
