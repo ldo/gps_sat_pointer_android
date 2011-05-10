5bd9303ee0eb3043424ad7b12deb7894be1bfb21
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2011-05-10 03:20:48 +0000

    label arrows

--- src/VectorView.java
+++ src/VectorView.java
@@ -39,6 +39,26 @@ class GraphicsUseful
             ThePaint;
       } /*FillWithColor*/
 
+    public static void DrawCenteredText
+      (
+        android.graphics.Canvas Draw,
+        String TheText,
+        android.graphics.PointF Where,
+        android.graphics.Paint UsePaint
+      )
+      /* draws text at position x, vertically centred around y. */
+      {
+        final android.graphics.Rect TextBounds = new android.graphics.Rect();
+        UsePaint.getTextBounds(TheText, 0, TheText.length(), TextBounds);
+        Draw.drawText
+          (
+            TheText,
+            Where.x, /* depend on UsePaint to align horizontally */
+            Where.y - (TextBounds.bottom + TextBounds.top) / 2.0f,
+            UsePaint
+          );
+      } /*DrawCenteredText*/
+
   } /*GraphicsUseful*/
 
 public class VectorView extends android.view.View
@@ -167,6 +187,31 @@ public class VectorView extends android.view.View
         invalidate();
       } /*FlashSat*/
 
+    Vec3f OurDirection
+      (
+        float Azimuth,
+        float Elevation
+      )
+     /* returns direction vector corresponding to Azimuth and Elevation in phone coordinates. */
+      {
+        Vec3f D;
+        final float AziCos = FloatMath.cos(Azimuth);
+        final float AziSin = FloatMath.sin(Azimuth);
+        final float ElevCos = FloatMath.cos(Elevation);
+        final float ElevSin = FloatMath.sin(Elevation);
+        D = OrientMatrix.xform
+          (
+            new Vec3f
+              (
+                  AziSin * ElevCos,
+                  - AziCos * ElevCos,
+                  ElevSin
+              )
+          );
+        return 
+            D;
+      } /*OurDirection*/
+
     android.graphics.Path PointTo
       (
         float Azimuth,
@@ -178,22 +223,7 @@ public class VectorView extends android.view.View
       /* returns an arrow path pointing in the specified absolute
         direction adjusted for phone coordinates. */
       {
-        Vec3f D; /* direction in phone coordinates */
-          {
-            final float AziCos = FloatMath.cos(Azimuth);
-            final float AziSin = FloatMath.sin(Azimuth);
-            final float ElevCos = FloatMath.cos(Elevation);
-            final float ElevSin = FloatMath.sin(Elevation);
-            D = OrientMatrix.xform
-              (
-                new Vec3f
-                  (
-                      AziSin * ElevCos,
-                      - AziCos * ElevCos,
-                      ElevSin
-                  )
-              );
-          }
+        final Vec3f D = OurDirection(Azimuth, Elevation);
         final android.graphics.Path V = new android.graphics.Path();
         final float Widen = 1.0f + D.z;
           /* taper to simulate perspective foreshortening */
@@ -234,6 +264,25 @@ public class VectorView extends android.view.View
             V;
       } /*PointTo*/
 
+    android.graphics.PointF PointAt
+      (
+        float Azimuth,
+        float Elevation,
+        float Radius
+      )
+      /* returns the coordinates where PointTo places the tip of the arrow. */
+      {
+        final Vec3f D = OurDirection(Azimuth, Elevation);
+        final float[] Result = {0.0f, + Radius};
+        final android.graphics.Matrix Orient = new android.graphics.Matrix();
+        Orient.postScale(1.0f, (float)Math.hypot(D.x, D.y));
+          /* perspective foreshortening factor */
+        Orient.postRotate(GraphicsUseful.ToDegrees((float)Math.atan2(- D.x, D.y)));
+        Orient.mapPoints(Result);
+        return
+            new android.graphics.PointF(Result[0], Result[1]);
+      } /*PointAt*/
+
     @Override
     protected void onDraw
       (
@@ -252,10 +301,21 @@ public class VectorView extends android.view.View
             /*useCenter =*/ false,
             /*paint =*/ GraphicsUseful.FillWithColor(0xff0a6d01)
           );
+        final android.graphics.Paint TextPaint = GraphicsUseful.FillWithColor(0xff887f04);
+        TextPaint.setTextSize(28.0f);
+        TextPaint.setTextAlign(android.graphics.Paint.Align.CENTER);
+        TextPaint.setAntiAlias(true);
         Draw.drawPath /* show direction of north */
           (
             PointTo(0.0f, 0.0f, Radius, true),
             GraphicsUseful.FillWithColor(0xff48c1af)
+          );
+        GraphicsUseful.DrawCenteredText
+          (
+            /*Draw =*/ Draw,
+            /*TheText =*/ "N",
+            /*Where =*/ PointAt(0.0f, 0.0f, Radius),
+            /*UsePaint =*/ TextPaint
           );
         for (SatInfo ThisSat : Sats)
           {
@@ -269,6 +329,13 @@ public class VectorView extends android.view.View
                     :
                         0xffeedf09
                   )
+              );
+            GraphicsUseful.DrawCenteredText
+              (
+                /*Draw =*/ Draw,
+                /*TheText =*/ String.format("%d", ThisSat.Prn),
+                /*Where =*/ PointAt(ThisSat.Azimuth, ThisSat.Elevation, Radius),
+                /*UsePaint =*/ TextPaint
               );
           } /*for*/
         Draw.restore();
