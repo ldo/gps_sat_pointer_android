57ebe42a1716c5c34c8cd14277d059f31c696773
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2011-10-31 00:01:34 +0000

    add broadcast receiver to return current GPS time offset

---
+++ src/GetTimeOffset.java
@@ -0,0 +1,32 @@
+package nz.gen.geek_central.GPSTest;
+/*
+    Respond to request for offset of system time from GPS time
+*/
+
+public class GetTimeOffset extends android.content.BroadcastReceiver
+  {
+
+    @Override
+    public void onReceive
+      (
+        android.content.Context Ctx,
+        android.content.Intent What
+      )
+      {
+        System.err.printf("GetTimeOffset received Intent %s\n", What.getAction()); /* debug */
+      /* don't bother checking received Intent, because I only perform one action anyway */
+      /* unfortunately BroadcastReceiver.PendingResult and goAsync are only available
+        in API level 11 or later */
+        if (Global.GotTimeDiscrepancy)
+          {
+            final android.os.Bundle Result = getResultExtras(true);
+            Result.putDouble("offset_from_gps", Global.TimeDiscrepancy / 1000.0);
+            setResultCode(android.app.Activity.RESULT_OK);
+          }
+        else
+          {
+            setResultCode(android.app.Activity.RESULT_FIRST_USER);
+          } /*if*/
+      } /*onReceive*/
+
+  } /*GetTimeOffset*/
